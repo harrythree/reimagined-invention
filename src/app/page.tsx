c2a7e3e0fc5654c50c3d74f460954b214f99916c
harry baldwin <harry.baldwiniii@endava.com>
2025-09-11 11:32:44 -0400

    added authenticator UI component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+// import Image from "next/image";
 
-export default function Home() {
-  return <div>This is stuff that has been updated</div>;
+// export default function Home() {
+//   return <div>This is stuff that has been updated</div>;
+// }
+
+"use client";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "../../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
